2433c6bfdc224f2e42cb812a128fc2585305659a
LevaniIlashvili <levaniko900@gmail.com>
2024-02-29 12:18:48 +0400

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import HistoryPage from "./pages/HistoryPage";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/history" element={<HistoryPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
