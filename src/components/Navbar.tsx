923539cd841a183b7860b830cf875f78110e353c
LevaniIlashvili <levaniko900@gmail.com>
2024-02-29 16:24:05 +0400

    implement navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,26 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <header className="p-2 px-6 shadow-md flex justify-between">
+      <Link to="/" className="text-4xl font-semibold">
+        Galery
+      </Link>
+      <nav className="flex items-center">
+        <ul className="flex gap-4">
+          <li>
+            <Link to="/" className="hover:font-medium">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/history" className="hover:font-medium transition-all">
+              History
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div></div>
+    </header>
+  );
 }
