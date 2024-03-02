49521d57cc4a59ecf1affc0b5357e31730d04c66
LevaniIlashvili <levaniko900@gmail.com>
2024-03-02 16:23:21 +0400

    implement history page

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -1,3 +1,38 @@
-export default function HistoryPage() {
-  return <div>HistoryPage</div>;
+import { useState } from "react";
+import { Photo } from "../../types";
+import { fetchSearchResults } from "../apiUtils";
+import Gallery from "../components/Gallery";
+
+export default function HistoryPage({
+  setOpenedPhoto,
+}: {
+  setOpenedPhoto: React.Dispatch<React.SetStateAction<string | null>>;
+}) {
+  const [photos, setPhotos] = useState<Photo[]>([]);
+
+  const history: string[] = JSON.parse(
+    localStorage.getItem("searchHistory") || "[]"
+  );
+
+  if (!history.length) return <div>No search history</div>;
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Search History</h1>
+      <ul>
+        {history.map((query, index) => (
+          <li
+            key={index}
+            className="w-96 cursor-pointer p-2
+          hover:bg-gray-200 rounded-md transition-colors duration-200 ease-in-out
+          "
+            onClick={() => fetchSearchResults(query, setPhotos)}
+          >
+            {query}
+          </li>
+        ))}
+        <Gallery photos={photos} setOpenedPhoto={setOpenedPhoto} />
+      </ul>
+    </div>
+  );
 }
