d72637bae7b1f9ae85fb7c768c037f17dbb64a7b
LevaniIlashvili <levaniko900@gmail.com>
2024-03-02 16:23:02 +0400

    move photo modal to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,27 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import HistoryPage from "./pages/HistoryPage";
 import Navbar from "./components/Navbar";
+import { useState } from "react";
+import PhotoModal from "./components/PhotoModal";
 
 function App() {
+  const [openedPhoto, setOpenedPhoto] = useState<string | null>(null);
+
   return (
     <Router>
+      {openedPhoto && (
+        <PhotoModal photoId={openedPhoto} setOpenedPhoto={setOpenedPhoto} />
+      )}
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/history" element={<HistoryPage />} />
+        <Route
+          path="/"
+          element={<HomePage setOpenedPhoto={setOpenedPhoto} />}
+        />
+        <Route
+          path="/history"
+          element={<HistoryPage setOpenedPhoto={setOpenedPhoto} />}
+        />
       </Routes>
     </Router>
   );
